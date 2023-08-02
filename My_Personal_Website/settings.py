82a0e50ffbbf3b9d164e2da3ec71933b3911bb4a
godeytech <godfredtenkorang9@gmail.com>
2023-08-02 00:54:30 +0000

    added postgresql

--- My_Personal_Website/settings.py
+++ My_Personal_Website/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'godey',
-        'USER': 'godey',
+        'USER': 'admin',
         'PASSWORD': 'Gony@100',
         'HOST': 'localhost',
         'PORT': '5432',
