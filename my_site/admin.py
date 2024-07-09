930350a6ea0077f6786f6b509784eac4110cfef3
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2024-07-09 15:29:35 +0000

    changes

--- my_site/admin.py
+++ my_site/admin.py
@@ -2,8 +2,28 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Contact)
+# admin.site.register(Contact)
 admin.site.register(Article)
-admin.site.register(Blog)
-admin.site.register(Category)
-admin.site.register(Portfolio)
+# admin.site.register(Blog)
+# admin.site.register(Category)
+# admin.site.register(Portfolio)
+
+class PortfolioInLine(admin.TabularInline):
+    model = Portfolio
+    extra = 3
+    
+  
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['name']})]
+    inlines = [PortfolioInLine]
+  
+  
+admin.site.register(Category, CategoryAdmin)
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone', 'message']
+    
+@admin.register(Blog)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['article', 'title', 'image', 'content', 'date_posted']
