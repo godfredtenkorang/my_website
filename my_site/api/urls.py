a4b2aa5a4dc8dc6999b91b5d6c840bce5cf13382
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2024-09-29 17:02:06 +0000

    changes

--- my_site/api/urls.py
+++ my_site/api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.blog_view)
+    path('', views.blog_view),
+    path('blog/<int:id>/', views.get_blog),
 ]
--- my_site/api/views.py
+++ my_site/api/views.py
@@ -2,10 +2,22 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from my_site.models import Blog
 from .serializers import BlogSerializer
+from rest_framework import status
 
 @api_view(['GET'])
 def blog_view(request):
     if request.method == 'GET':
         blogs = Blog.objects.all()
         serializer = BlogSerializer(blogs, many=True)
+        return Response(serializer.data)
+    
+@api_view(['GET'])
+def get_blog(request, id):
+    try:
+        blog = Blog.objects.get(id=id)
+    except Blog.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = BlogSerializer(blog)
         return Response(serializer.data)
